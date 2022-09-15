868c42c32a6a2536b7d9085a302a90ce054e122c
ned1313 <ned.bellavance@gmail.com>
2022-09-15 15:01:36 -0400

    add new output

--- outputs.tf
+++ outputs.tf
@@ -3,3 +3,8 @@ output "service_principal" {
   value = azuread_service_principal.oidc
   description = "The full service principal object associated with the application."
 }
+
+output "azuread_application" {
+  value = azuread_application.oidc
+  description = "The full application object associated with the service principal."
+}
